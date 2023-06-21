958dab8c7c000f55e61618f4347a255ee14b5e14
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-20 19:22:11 -0600

    section-4 pt1
    practica

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         HolaMundo hm = new HolaMundo();
         HolaMundo.imprimir(hm.mensaje);
+        HolaMundo.imprimirSuma(2,4);
     }
 }
--- Imprimir.java
+++ Imprimir.java
@@ -2,4 +2,9 @@ public class Imprimir {
     public static void imprimir(String mensaje) {
         System.out.println(mensaje);
     }
+
+    public static void imprimirSuma(int n, int m) {
+        double resultado = n+m; 
+        System.out.println(resultado);
+    }
 }
