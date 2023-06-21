552c4a721b947125300b2e7838614f99489d0d03
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-06-20 19:22:45 -0600

    section-4 pt2
    practica

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+public class Calculadora {
+    public double suma() {
+        return 0;
+    }
+    public double resta() {
+        return 0;
+    }
+    public double division() {
+        return 0;
+    }
+    public double multiplicacion() {
+        return 0;
+    }
+}
